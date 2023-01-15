5204c616538976471ce6801dc8b16e1c0cf38ad5
blackbriarmead <apierson1141@gmail.com>
2023-01-14 20:08:49 -0600

    added persistence capabilities to Mongodb, added health check and dependency in docker-compose

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //https://blog.logrocket.com/packaging-a-rust-web-service-using-docker/
+//docker build -t rust-debian -f Dockerfile .
 
+use mongodb;
 use warp::{Filter, Rejection, Reply};
 
 type Result<T> = std::result::Result<T, Rejection>;
@@ -8,12 +10,27 @@ type Result<T> = std::result::Result<T, Rejection>;
 async fn main() {
     let health_route = warp::path!("health").and_then(health_handler);
 
-    let routes = health_route.with(warp::cors().allow_any_origin());
+    let retrieve_activity_route =
+        warp::path!("retrieve_activity").and_then(retrieve_activity_handler);
+    let upload_activity_route = warp::path!("upload_activity").and_then(upload_activity_handler);
+
+    let routes = health_route
+        .or(retrieve_activity_route)
+        .or(upload_activity_route)
+        .with(warp::cors().allow_any_origin());
 
     println!("Started server at localhost:8000");
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
 
 async fn health_handler() -> Result<impl Reply> {
+    Ok("OK")
+}
+
+async fn retrieve_activity_handler() -> Result<impl Reply> {
+    Ok("OK")
+}
+
+async fn upload_activity_handler() -> Result<impl Reply> {
     Ok("OK")
 }
